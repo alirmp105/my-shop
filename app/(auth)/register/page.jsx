5b18180fd8e907e1ca44d40b6157f67a17307139
ali <alirmp105@gmail.com>
2026-09-06 20:22:02 +0330

    debug session for register page

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -2,8 +2,8 @@ import RegisterForm from "@/components/auth/RegisterForm";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-const RegisterPage = () => {
-  const session = getServerSession(authOptions);
+const RegisterPage = async () => {
+  const session = await getServerSession(authOptions);
     if(session){
       redirect("/profile")
     }
